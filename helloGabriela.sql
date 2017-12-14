72ddec3d0731a9b4fe4d58250c7c3f1bc9978441
Neelesh Shah <NeeleshShah@gmail.com>
2017-12-14 18:26:59 -0500

    Messy comment

--- helloGabriela.sql
+++ helloGabriela.sql
@@ -1,5 +1,6 @@
 create or replace procedure helloGabriela is
 begin
+   -- Now it is getting messy
    htp.p ('Hello Gabriela');
    dbms_output.put_line('Hello Gabriela');
    -- This is a comment - YEAH !
