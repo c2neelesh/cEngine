7b464640f333e5b1f55c6f7610214689cfebf2cf
c2 Neelesh <neelesh@concept2completion.com>
2017-12-14 18:24:35 -0500

    One more comment made by someone else

--- helloGabriela.sql
+++ helloGabriela.sql
@@ -3,4 +3,5 @@ begin
    htp.p ('Hello Gabriela');
    dbms_output.put_line('Hello Gabriela');
    -- This is a comment - YEAH !
+   -- This is one more comment made by someone else !!
 end helloGabriela;
