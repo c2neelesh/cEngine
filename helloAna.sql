7fdf3fd308a5498a21cc52151933f8953757982e
c2 Neelesh <neelesh@concept2completion.com>
2017-12-15 08:42:02 -0500

    Chop Chop added to helloAna

--- helloAna.sql
+++ helloAna.sql
@@ -1,6 +1,6 @@
 create or replace procedure helloAna is
 begin
-   htp.p ('WTA rocks'); -- this line is new
+   htp.p ('WTA rocks'); -- this line is new, Chop Chop
    htp.p ('Hello Ana');
    dbms_output.put_line('Hello Ana');
    -- Yo yo Ana.
